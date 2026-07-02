dbe26ad4c65937270c22cab27f9a62e527e4701a
channelalfin3-pixel <channelalfin3@gmail.com>
2026-07-02 11:29:12 +0700

    Create api.js

---
+++ js/js/api.js
@@ -0,0 +1,17 @@
+async function postAPI(data){
+
+const response = await fetch(CONFIG.API_URL,{
+
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify(data)
+
+});
+
+return await response.json();
+
+}
